5b22d66403532fc2a2885dc19f6e3c48e8636342
shivanshu sood <shvnshsood@gmail.com>
2022-11-17 13:11:26 +0530

    Add post updated

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const  navItems = document.querySelector('.nav__items');
+const  openNavBtn = document.querySelector('#open__nav-btn');
+const  closeNavBtn = document.querySelector('#close__nav-btn');
+
+
+//opens nav dropdrown 
+const openNav = () => {
+
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block' ;
+}
+// close nav dropdrown
+    const closeNav = () => {
+
+        navItems.style.display = 'none';
+        openNavBtn.style.display = 'inline-block';
+        closeNavBtn.style.display = 'none';
+    
+    }
+    
+
+
+openNavBtn.addEventListener('click' , openNav);
+
+closeNavBtn.addEventListener('click' ,closeNav);
+
+
+
+// sidebar
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+
+//show
+const showSidebar  = ()=>{
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display='inline-block';
+}
+
+
+
+//hide
+const hideSidebar  = ()=>{
+    sidebar.style.left = '-100%';
+    hideSidebarBtn.style.display = 'none';
+   showSidebarBtn.style.display='inline-block';
+}
+
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click',hideSidebar);
